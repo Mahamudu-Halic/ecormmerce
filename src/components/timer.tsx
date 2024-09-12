6a284b9a8fcbc9649b71d97a7e0fc633cb69eee1
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-12 06:19:09 +0000

    fixed timer created

---
+++ src/components/timer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const TimerComponent = () => {
+  return (
+    <div className="z-50 bg-black text-white w-full p-3 flex items-center justify-center gap-3 sticky top-0">
+      New Season Coming! Discount 10% for all product! Check out now! <span className="bg-[#2F2F2F] py-1 px-2 rounded-xl text-xs">20:48</span>
+    </div>
+  );
+};
+
+export default TimerComponent;
