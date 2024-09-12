c0e61f45e00eab2cfe297e2c699ddd3442f9b786
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-12 06:18:01 +0000

    navbar created

---
+++ src/components/navbar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { NavItems } from "../../constant";
+import Link from "next/link";
+import { BriefcaseBusiness, CircleUserRound, Menu, Search } from "lucide-react";
+import MobileSidebar from "./mobile-sidebar";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar((prev) => !prev);
+  };
+  return (
+    <div className="flex justify-between items-center p-5">
+      <div className="flex items-center gap-5">
+        <h2 className="font-extrabold text-3xl">Jolt</h2>
+        <div className="md:flex items-center gap-5 capitalize hidden">
+          {NavItems.map(
+            (item: { name: string; url: string; active?: boolean }) => {
+              return (
+                <Link
+                  href={item?.url}
+                  key={item?.name}
+                  className={` text-lg ${
+                    item?.active ? "text-black font-bold" : "text-[#b8b6b6]"
+                  }`}
+                >
+                  {item?.name}
+                </Link>
+              );
+            }
+          )}
+        </div>
+      </div>
+      <div className="flex items-center gap-5">
+        <button className="flex items-center gap-3 font-bold">
+          <Search />
+          <p className="hidden md:block">Search</p>
+        </button>
+        <button className="flex items-center gap-3 font-bold">
+          <BriefcaseBusiness />
+          <p className="hidden md:block">Cart</p>
+        </button>
+        <button className="md:flex items-center gap-3 font-bold hidden">
+          <CircleUserRound />
+          Login
+        </button>
+        <Menu className="md:hidden cursor-pointer" onClick={toggleSidebar} />
+      </div>
+
+      <MobileSidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
+    </div>
+  );
+};
+
+export default Navbar;
