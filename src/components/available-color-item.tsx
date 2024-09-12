f0c0407723c53fcb5b608fbd10a19733a51bdb41
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-12 06:20:41 +0000

    available colors component created

---
+++ src/components/available-color-item.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AvailableColorItem = ({
+  color,
+  selectedColor,
+  changeSelectedColor,
+}: {
+  color: string;
+  selectedColor: string;
+  changeSelectedColor: (value: string) => void;
+}) => {
+  return (
+    <button
+      className={`border ${
+        selectedColor === color ? "border-black" : "border-[#b6b8b8]"
+      } rounded-lg p-1`}
+      onClick={() => changeSelectedColor(color)}
+    >
+      <span
+        className={"w-7 h-7 rounded-lg block"}
+        style={{ background: color }}
+      ></span>
+    </button>
+  );
+};
+
+export default AvailableColorItem;
