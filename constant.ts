0643398e3f8a3f3750225e6f2a12731909753cc7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-12 06:16:17 +0000

    const values and dummy data

---
+++ constant.ts
@@ -0,0 +1,69 @@
+import { BriefcaseBusiness, CircleUserRound, Search } from "lucide-react";
+
+export const NavItems = [
+  {
+    name: "categories",
+    url: "/categories",
+    active: true,
+  },
+  {
+    name: "collections",
+    url: "/collections",
+  },
+  {
+    name: "store",
+    url: "/store",
+  },
+  {
+    name: "blog",
+    url: "/blog",
+  },
+  {
+    name: "find store",
+    url: "/find-store",
+  },
+];
+
+export const ItemInfo = {
+  category: "Jolt / Jakets",
+  name: "Boa Fleece Jacket",
+  old_price: 129,
+  current_price: 122,
+  discount: 5,
+  stars: 4,
+  reviews: "12K",
+
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+};
+
+export const Colors = ["#121212", "#0101AF", "#113C11"];
+
+export const Sizes = ["S", "M", "L", "XL"];
+
+export const RecommendedItemsList = [
+  {
+    name: "Adidas",
+    price: 349,
+    favorite: true,
+    image: "/assets/img05 1.png",
+  },
+  {
+    name: "P.S Cap",
+    price: 49,
+    favorite: true,
+    image: "/assets/img05 1.png",
+  },
+  {
+    name: "Adidas",
+    price: 36,
+    favorite: false,
+    image: "/assets/img05 1.png",
+  },
+  {
+    name: "Adidas",
+    price: 56,
+    favorite: false,
+    image: "/assets/img05 1.png",
+  },
+];
