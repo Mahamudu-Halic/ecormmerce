22cbb904d161edaa164fc0293a885790d8e21b28
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-12 06:21:34 +0000

    image list item creted

---
+++ src/components/item-image-list.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+const ItemImageList = () => {
+  return (
+    <div className="flex lg:flex-col gap-3 justify-between">
+      {Array.from({ length: 4 }).map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="relative lg:w-[120px] md:w-full w-[100px] lg:h-full md:h-[100px] h-[100px] rounded-xl bg-[#F2F2F2]"
+          >
+            <Image
+              fill
+              src={"/assets/img05 1.png"}
+              alt="item"
+              className="object-center"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ItemImageList;
